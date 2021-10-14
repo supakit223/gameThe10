8b4d05757359ff2da78198526f046187d855422e
supakit223 <supakit223@gmail.com>
2021-10-14 19:59:13 +0700

    first commit

--- script.js
+++ script.js
@@ -1,44 +1,43 @@
 function cal() {
-    const numberArray = [1,2,3,4,5,6,7,8,9]
+    const numberArray = [1,2,3,4,5,6,7,8,9,10]
     const result = getPermutation(numberArray)        
     let number = ""
     let count = 0
-    const sum = document.getElementById("sum").value
-        
-    for (let i = 0 ; i < 362880 ; i++) { 
-        if(result[i][0]+result[i][1]+result[i][2] == sum && result[i][3]+result[i][4]+result[i][5] == sum && result[i][6]+result[i][7]+result[i][8] == sum
-        && result[i][0]+result[i][3]+result[i][6] == sum && result[i][1]+result[i][4]+result[i][7] == sum && result[i][2]+result[i][5]+result[i][8] == sum
-        && result[i][0]+result[i][4]+result[i][8] == sum && result[i][2]+result[i][4]+result[i][6] == sum){                 
-            // number = number + `<div class="mt-3 d-flex align-content-center justify-content-center">
-            //     <input id="1" class="p-1 mx-2 input" type="number" min="1" max="9" placeholder="00">
-            //     <input id="2" class="p-1 mx-2 input" type="number" min="1" max="9" placeholder="01">
-            //     <input id="3" class="p-1 mx-2 input" type="number" min="1" max="9" placeholder="02">
-            // </div>
-            // <div class="mt-3 d-flex align-content-center justify-content-center">
-            //     <input id="4" class="p-1 mx-2 input" type="number" min="1" max="9" placeholder="03">
-            //     <input id="5" class="p-1 mx-2 input" type="number" min="1" max="9" placeholder="04">
-            //     <input id="6" class="p-1 mx-2 input" type="number" min="1" max="9" placeholder="05">
-            // </div>
-            // <div class="mt-3 d-flex align-content-center justify-content-center">
-            //     <input id="7" class="p-1 mx-2 input" type="number" min="1" max="9" placeholder="06">
-            //     <input id="8" class="p-1 mx-2 input" type="number" min="1" max="9" placeholder="07">
-            //     <input id="9" class="p-1 mx-2 input" type="number" min="1" max="9" placeholder="08">
-            // </div>`
-            for(let j = 0 ; j < 9 ; j++){
-                count++
-                number = number + result[i][j] +"     "
-                if (count%3 == 0){
-                    number = number + "<br>"
-                }       
-                if (count%9 == 0){
-                    number = number + "<br>"
-                }                        
-            }            
-        }     
+    const sum = document.getElementById("sum").value        
+    for (let i = 0 ; i < 3628800 ; i++) { 
+        if(result[i][0]+result[i][1]+result[i][2] == sum && result[i][7]+result[i][8]+result[i][9] == sum 
+        && result[i][0]+result[i][3]+result[i][5]+result[i][7] == sum && result[i][2]+result[i][4]+result[i][6]+result[i][9] == sum){
+            count++
+            number = number + 
+            `
+            <div class="d-flex justify-content-center align-content-center">
+                <label class="groupResult">ชุดที่ ${count}</label>
+            </div>
+            <div class="mt-3 d-flex align-content-center justify-content-center">
+                <input id="1" class="p-1 mx-2 input" type="number" min="1" max="10" value=${result[i][0]}>
+                <input id="2" class="p-1 mx-2 input" type="number" min="1" max="10" value=${result[i][1]}>
+                <input id="3" class="p-1 mx-2 input" type="number" min="1" max="10" value=${result[i][2]}>
+            </div>           
+            <div class="mt-3 d-flex align-content-center justify-content-center">
+                <input id="4" class="p-1 mx-5 input" type="number" min="1" max="10" value=${result[i][3]}>
+                <label for="">...</label>
+                <input id="5" class="p-1 mx-5 input" type="number" min="1" max="10" value=${result[i][4]}>                    
+            </div>           
+            <div class="mt-3 d-flex align-content-center justify-content-center">
+                <input id="6" class="p-1 mx-5 input" type="number" min="1" max="10" value=${result[i][5]}>
+                <label for="">...</label>
+                <input id="7" class="p-1 mx-5 input" type="number" min="1" max="10" value=${result[i][6]}>                    
+            </div>           
+            <div class="mt-3 d-flex align-content-center justify-content-center">
+                <input id="8" class="p-1 mx-2 input" type="number" min="1" max="10" value=${result[i][7]}>
+                <input id="9" class="p-1 mx-2 input" type="number" min="1" max="10" value=${result[i][8]}>
+                <input id="10" class="p-1 mx-2 input" type="number" min="1" max="10" value=${result[i][9]}>
+            </div><br>          
+            `                       
+        }           
     }
     const inpObj = document.getElementById("sum")
     if (!inpObj.checkValidity()) {
-
         document.getElementById("alert").classList.remove("invisible")
         document.getElementById("alert").classList.add("visible")
         
@@ -47,10 +46,8 @@ function cal() {
         document.getElementById("output2").classList.remove("visible")
         document.getElementById("output2").classList.add("invisible")
 
-        document.getElementById("alert").innerHTML = inpObj.validationMessage
-
-        
-    }else if(count == 0) {        
+        document.getElementById("alert").innerHTML = inpObj.validationMessage        
+    }else if(count == 0) {           
         document.getElementById("output1").classList.remove("invisible")
         document.getElementById("output1").classList.add("visible")
         document.getElementById("output2").classList.remove("visible")
@@ -67,7 +64,7 @@ function cal() {
         document.getElementById("alert").classList.remove("visible")
         document.getElementById("alert").classList.add("invisible")
 
-        document.getElementById("output1").innerHTML = `มี ${count/9} ชุดคำตอบ ดังนี้`
+        document.getElementById("output1").innerHTML = `มี ${count} ชุดคำตอบ ดังนี้`
         document.getElementById("output2").innerHTML = number
     }    
 }
